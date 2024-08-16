c05aa03e7e90ac5b6bd73b40343c6e61cf64db59
zfq <137599793+chinese-coding@users.noreply.github.com>
2024-08-16 15:52:14 +0800

    feat(opencood): 实现对训练配置 (YAML 文件) 加载的功能
    DETAILS:
    1. 实现对训练配置 (YAML 文件) 加载的功能. 实现该部分功能时并没有完全按照参考项目结构.
    2. 以 OPV2V 数据集下的 HEAL 的 stage1/m1_pyramid.yaml 为测试对象, 对这部分功能进行了测试, 测试通过.
    
    TODO:
    1. 目前只抄写了 `load_general_params` 这么一个加载参数的函数, 后续可以把其他函数抄过来 (尽管其他函数的使用频率不是那么大).
    2. 修改配置文件中某些项的名称, 使得代码中的名称合配置项的名称一致, 增强可读性 (这一点实现起来比较困难, 同时也比较鸡肋).

---
+++ opencood/tools/train.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+
+from icecream import ic
+
+from opencood.utils.yaml_utils import load_yaml
+
+
+
+def args_parser():
+    """
+    这段函数可以直接从命令行中读取参数, 而无需传参
+    Returns:
+    解析的命令行参数
+    """
+    parser = argparse.ArgumentParser(description="synthetic data generation")
+    parser.add_argument("--config", type=str, required=True, help='data generation yaml file needed')
+    parser.add_argument('--model_dir', default='', help='Continued training path')
+    parser.add_argument('--fusion_method', '-f', default="intermediate", help='passed to inference.')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = args_parser()
+    configs = load_yaml(args.config, args.model_dir)
+    ic(configs)  # 测试专用
+
+
+if __name__ == '__main__':
+    sys.argv = ["train.py", "--config", "../../opencood/configs/OPV2V/MoreModality/HEAL/stage1/m1_pyramid.yaml"]
+    main()
---
+++ opencood/utils/yaml_utils.py
@@ -0,0 +1,73 @@
+import math
+import os
+import re
+from typing import Dict
+
+import yaml
+
+from logger import get_logger
+
+logger = get_logger()
+
+pattern = """^(?:
+         [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+        |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+        |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+        |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+        |[-+]?\\.(?:inf|Inf|INF)
+        |\\.(?:nan|NaN|NAN))$"""
+
+
+def load_yaml(file: str, model_dir=None):
+    """
+    加载原始配置文件, 并根据原始配置文件的要求对配置文件的内容进行修改
+    :param file:
+    :param model_dir:
+    :return:
+    """
+    if model_dir:
+        # WARNING: 如果从 `model_dir` 文件加载时就是从 `config.yaml` 中加载, 所以一定要注意命名问题.
+        file = os.path.join(model_dir, 'config.yaml')
+    try:
+        with open(file, 'r') as stream:
+            loader = yaml.Loader
+            loader.add_implicit_resolver('tag:yaml.org,2002:float', re.compile(pattern, re.X), list('-+0123456789.'))
+            configs = yaml.load(stream, Loader=loader)
+    except FileNotFoundError:
+        logger.error(f"{file} 没有找到")
+    except yaml.YAMLError as e:
+        logger.error(f"解析文件时出错: {e}")
+
+    if "yaml_parser" in configs:
+        # TODO: 修改这种 `eval` 函数的写法
+        configs = eval(configs["yaml_parser"])(configs)
+    return configs
+
+
+def load_general_params(configs: Dict) -> Dict:
+    """
+    Based on the lidar range and resolution of voxel, calculate the anchor box and target resolution.
+
+    :param configs: Original loaded parameter dictionary.
+    :return: Modified parameter dictionary with new attribute.
+    """
+    cav_lidar_range = configs['preprocess']['cav_lidar_range']
+    voxel_size = configs['preprocess']['args']['voxel_size']
+    anchor_args = configs['postprocess']['anchor_args']
+
+    vw = voxel_size[0]
+    vh = voxel_size[1]
+    vd = voxel_size[2]
+
+    anchor_args['vw'] = vw
+    anchor_args['vh'] = vh
+    anchor_args['vd'] = vd
+
+    # W is image width, but along with x axis in lidar coordinate
+    anchor_args['W'] = math.ceil((cav_lidar_range[3] - cav_lidar_range[0]) / vw)
+    anchor_args['H'] = math.ceil((cav_lidar_range[4] - cav_lidar_range[1]) / vh)  # H is image height
+    anchor_args['D'] = math.ceil((cav_lidar_range[5] - cav_lidar_range[2]) / vd)
+
+    configs['postprocess'].update({'anchor_args': anchor_args})
+
+    return configs
