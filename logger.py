f7519780fad3176180f1df15e16e8d1adb04c08e
zfq <137599793+chinese-coding@users.noreply.github.com>
2024-08-17 08:03:47 +0800

    feat: 实现了自己的 Logger 类
    DETAILS (Logger 类增加的新功能):
    1. 在原先 loguru 的基础上实现了 `important` 级别, 即在任何情况下该信息均会输出 (类似 print, 但使用了 loguru 来实现, 这样命令行输出比较统一).
    2. 增加了 `critical` 级别的功能, 在原先输出错误信息的基础上直接结束程序, 这样也更加符合 `critical` 级别的定义.
    2. 使用 python 提供的函数装饰器功能, 能够正确输出调用 logger 所在的文件以及行号, 弥补了之前 Logger 的问题 (没有在提交过之前 Logger 的代码).
    
    TODO:
    1. 该 Logger 对于 python 开发具有普遍的意义, 应该找时间专门写个博客记录一下, 特别是输出正确文件名和行号这部分.
      因为这部分经过近 1 个小时的网上搜索无果后, 最终求助于官方文档, 经官方文档的启发才编写出此代码, 足以看到官方文档的重要性.

--- logger.py
+++ logger.py
@@ -1,10 +1,75 @@
+import functools
 import sys
+from loguru import logger as loguru_logger
 
-from loguru import logger
+# 定义自定义的日志等级
+loguru_logger.level("IMPORTANT", no=100, color="<blue><bold><italic>")
 
-logger.remove()  # 删除默认的
-logger.add(sys.stdout, level="SUCCESS")
+def logger_wraps(level):
+    def wrapper(func):
+        @functools.wraps(func)
+        def wrapped(self, *args, **kwargs):
+            # 提取第二个参数作为日志消息
+            message = args[0] if len(args) > 0 else ""
+            self._log(level, message)
+            return func(self, *args, **kwargs)  # 确保原始方法仍然被调用
+        return wrapped
+    return wrapper
 
+class Logger:
+    def __init__(self):
+        self.logger = loguru_logger
+
+    def _log(self, level, message):
+        # 通用的日志记录方法
+        logger_ = self.logger.opt(depth=2)
+        logger_.log(level, message)
+
+    @logger_wraps(level="TRACE")
+    def trace(self, message):
+        pass
+
+    @logger_wraps(level="DEBUG")
+    def debug(self, message):
+        pass
+
+    @logger_wraps(level="INFO")
+    def info(self, message):
+        pass
+
+    @logger_wraps(level="SUCCESS")
+    def success(self, message):
+        pass
+
+    @logger_wraps(level="WARNING")
+    def warning(self, message):
+        pass
+
+    @logger_wraps(level="ERROR")
+    def error(self, message):
+        pass
+
+    @logger_wraps(level="CRITICAL")
+    def critical(self, message, exit_code=1):
+        sys.exit(exit_code)
+
+    @logger_wraps(level="IMPORTANT")
+    def important(self, message):
+        pass
+
+logger = Logger()
 
 def get_logger():
     return logger
+
+"""使用示例"""
+if __name__ == '__main__':
+    logger.info("This is an info message.")
+    logger.success("SUCCESS1111111")
+    logger.warning("WARNING")
+    logger.success("SUCCESS")
+    logger.error("ERROR")
+    logger.important("This is an important message that must always be output.")
+    logger.error("ERROR")
+    logger.critical("This is a critical error. Exiting the program.")
+    logger.important("This is an important message that must always be output.")
